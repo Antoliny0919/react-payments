3ebc40fdfe0380ffcc75412c5c5fcf377ac533bc
Antoliny0919 <antoliny0919@gmail.com>
2026-05-11 16:00:05 +0900

    Updates

---
+++ assets/CreateComplete-B6-iwPfD.js
@@ -0,0 +1,20 @@
+import{D as e,k as t,t as n}from"./jsx-runtime-BU7ublGc.js";import{a as r,n as i,r as a}from"./Button-D74B3IW6.js";var o=`data:image/svg+xml,%3csvg%20width='76'%20height='76'%20viewBox='0%200%2076%2076'%20fill='none'%20xmlns='http://www.w3.org/2000/svg'%3e%3ccircle%20cx='38'%20cy='38'%20r='38'%20fill='%23333333'/%3e%3cpath%20d='M23%2034.0377L35.0471%2048L55%2028'%20stroke='white'%20stroke-width='7.49999'%20stroke-linecap='round'%20stroke-linejoin='round'/%3e%3c/svg%3e`,s=n();function c(){let{firstDigitsCardNumber:t,cardBrand:n}=e().state??{};return(0,s.jsx)(l,{children:(0,s.jsxs)(d,{children:[(0,s.jsx)(u,{src:o,alt:`card-create-complete`}),(0,s.jsxs)(`p`,{children:[t,`로 시작하는 `,a[n].title,`카드 가 등록되었어요.`]}),(0,s.jsx)(i,{type:`button`,children:`확인`})]})})}var l=r.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  padding: 0 19px;
+`,u=r.img`
+  height: 76px;
+  width: 76px;
+`,d=r.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 25px;
+  p {
+    font-size: 25px;
+    color: #353c49;
+    font-weight: 700;
+  }
+`,f=t(function(){return(0,s.jsx)(c,{})});export{f as default};
